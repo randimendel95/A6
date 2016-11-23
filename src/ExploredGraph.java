723adfbeb2efeb8f45632a89fd629673e8022eeb
Riley Andert <Riley Andert>
2016-11-22 23:30:40 -0800

    Made sure retrieve path was working

--- src/ExploredGraph.java
+++ src/ExploredGraph.java
@@ -252,7 +252,7 @@ public class ExploredGraph {
         
         // Implement this. (Breadth-First Search)
         public LinkedList<Vertex> retrievePath(Vertex vj) {
-        	int pathLength = 0;
+        	int pathLength = -1;
             Stack<Vertex> temp = new Stack<Vertex>();
             LinkedList<Vertex> tempNodes = new LinkedList<Vertex>();
             tempNodes = Ve;
@@ -303,11 +303,13 @@ public class ExploredGraph {
                 Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
                 //System.out.println(v0);
                 Vertex v1 = eg.new Vertex("[],[],[4,3,2,1]");
+                eg.idfs(v0,v1);
+                eg.retrievePath(v1);
                 Vertex v2 = eg.new Vertex("[[],[4,3,1],[2]]");
                 //Operator op = eg.new Operator(0,1);
                 //eg.idfs(v0,v1);
-                eg.bfs(v0,v1);
-                eg.retrievePath(v2);
+                //eg.bfs(v0,v1);
+                //eg.retrievePath(v2);
                 //eg.retrievePath(v2);
                 // Add your own tests here.
                 // The autograder code will be used to test your basic functionality later.
